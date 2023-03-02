0c48a5bb090963ce9f7a1454400d6f2cec87d5df
Kirin <tuj17819@temple.edu>
2023-03-02 11:57:11 -0500

    Added logic and displayed the images

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -1,10 +1,10 @@
 package edu.temple.inclassactivity
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Spinner
-import android.widget.SpinnerAdapter
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +22,17 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Display images specified in imageArray in Spinner and RecyclerView
+        // display images in a spinner
+        val spinnerAdapter = CustomSpinnerAdapter(this, imageArray)
+        val recyclerAdapter = CustomRecyclerAdapter(imageArray)
 
+        if(spinner != null) {
+            spinner.adapter = spinnerAdapter
+        }
+        // display images in a recyclerView
+        if(recyclerView != null) {
+            recyclerView.adapter = recyclerAdapter
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        }
     }
 }
